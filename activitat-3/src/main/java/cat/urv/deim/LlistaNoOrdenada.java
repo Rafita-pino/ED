5fe20b7d17a06f83ee56ad5326b626036a4359c0
Rafita-pino <r.pinor04@gmail.com>
2025-02-11 16:38:17 +0100

    final commit

---
+++ activitat-3/src/main/java/cat/urv/deim/LlistaNoOrdenada.java
@@ -0,0 +1,134 @@
+package cat.urv.deim;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import cat.urv.deim.exceptions.ElementNoTrobat;
+import cat.urv.deim.exceptions.PosicioForaRang;
+
+public class LlistaNoOrdenada<E extends Comparable<E>> implements ILlistaGenerica<E> {
+    public int numElem;
+    public Node fantasma;
+    public class Node{
+        private E info;
+        private Node nextNode;
+
+        public Node(E e){
+            info = e;
+            nextNode = null;
+        }
+    }
+    public LlistaNoOrdenada(){
+        fantasma = new Node(null); //creem element fantasma
+        numElem = 0;
+
+    }
+
+    @Override
+    public void inserir(E e) {
+        Node newNode = new Node(e);
+        newNode.nextNode=fantasma.nextNode;
+        fantasma.nextNode=newNode;
+        numElem++;
+    }
+    @Override
+    public void esborrar(E e) throws ElementNoTrobat {
+        if (esBuida()) {
+            throw new ElementNoTrobat();
+        }else{
+            Node actual = fantasma.nextNode, ant=null;
+            while (actual != null && !actual.info.equals(e)){
+                ant = actual;
+                actual = actual.nextNode;
+            }
+            if (ant == null) {
+                fantasma=actual.nextNode; //eleminar el primer de la llista
+            }else if (actual != null && actual.info.equals(e)) {
+                ant.nextNode = actual.nextNode;
+            }else{
+                throw new ElementNoTrobat();
+            }
+            numElem -= 1;
+        }
+    }
+
+    @Override
+    public E consultar(int pos) throws PosicioForaRang {
+        if (pos<numElem || pos > numElem) {
+            throw new PosicioForaRang();
+        }else{
+            Node actual = fantasma;
+            for (int i = 0; i < pos; i++) {
+                actual = actual.nextNode;
+            }
+            return (E) actual.info;
+        }
+
+
+    }
+
+    @Override
+    public int buscar(E e) throws ElementNoTrobat {
+        int pos = 0;
+        Node actual = fantasma.nextNode;
+        while (!actual.info.equals(e) && pos <numElem){
+            pos++;
+            actual=actual.nextNode;
+        }
+        if (pos>=numElem) {
+            throw new ElementNoTrobat();
+        }
+        return pos;
+
+    }
+
+    @Override
+    public boolean existeix(E e) {
+        boolean trobat = false;
+        if (esBuida()) {
+            return trobat;
+        }else{
+            Node actual = fantasma.nextNode;
+            while (actual != null && !actual.info.equals(e)){
+                actual = actual.nextNode;
+            }
+            if (actual!=null  && actual.info.equals(e)) {
+                trobat = true;
+            }
+            return trobat;
+        }
+    }
+
+    @Override
+    public boolean esBuida() {
+        return (numElem==0);
+
+    }
+
+    @Override
+    public int numElements() {
+        return numElem;
+
+    }
+    @Override
+    public Iterator<E> iterator() {
+        return new LlistaIterator();
+    }
+
+    private class LlistaIterator implements Iterator<E> {
+        private Node actual = fantasma;
+
+        @Override
+        public boolean hasNext() {
+            return (actual.nextNode != null);
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return (actual.nextNode.info);
+        }
+    }
+}
---
+++ activitat-3/src/main/java/cat/urv/deim/LlistaPersones.java
@@ -0,0 +1,126 @@
+package cat.urv.deim;
+
+import cat.urv.deim.exceptions.ElementNoTrobat;
+import cat.urv.deim.exceptions.PosicioForaRang;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+
+public class LlistaPersones{
+    ILlistaGenerica<Persona> llista;
+
+    // Constructor que crea una llista de persones buida del tipus especificat en el boolea (ordenada o no)
+    public LlistaPersones(boolean ordenada) {
+        if (ordenada) {
+            llista = new LlistaOrdenada<Persona>();
+        }else{
+            llista = new LlistaNoOrdenada<Persona>();
+        }
+    }
+
+    //Constructor que crea una llista del tipus especificat i hi carrega totes les dades del fitxer
+    public LlistaPersones(boolean ordenada, String filename) throws IOException{
+        if (ordenada) {
+            llista = new LlistaOrdenada<Persona>();
+        }else{
+            llista = new LlistaNoOrdenada<Persona>();
+        }
+        try(BufferedReader r = new BufferedReader(new FileReader(filename))){
+            String line;
+            String [] parts;
+            Persona p;
+            while ((line = r.readLine())!= null) {
+                parts = line.split(",");
+                p = new Persona(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), parts[2], parts[3],
+                                Integer.parseInt(parts[4]), Integer.parseInt(parts[5]));
+                llista.inserir(p);
+            }
+
+        }   catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    //Afegeix una nova persona a la llista que tenim inicialitzada
+    public void inserir(Persona p) {
+        llista.inserir(p);
+    }
+
+    //Metode per a consultar una persona a partir de la seva posicio
+    public Persona consultar(int pos) throws PosicioForaRang {
+        return llista.consultar(pos);
+    }
+
+    //Metode per a saber si una persona existeix a l'estructura
+    public boolean existeix(Persona p) {
+        return llista.existeix(p);
+    }
+
+    //Metode per a esborrar una persona de l'estructura
+    public void esborrar(Persona e) throws ElementNoTrobat {
+        llista.esborrar(e);
+    }
+
+    //Metode que ens indica en quina posicio de la llista hi ha la persona que es passa per parametre
+    public int posicioPersona(Persona persona) throws ElementNoTrobat {
+        return llista.buscar(persona);
+    }
+
+    //Metode per a saber si la llista esta buida
+    public boolean esBuida() {
+        return llista.esBuida();
+    }
+
+    //Metode per a saber el nombre d'elements de la llista
+    public int numElements() {
+        return llista.numElements();
+    }
+
+    public Persona[] elements() {
+        int i = 0;
+        Persona[] elem = new Persona[llista.numElements()];
+        Iterator<Persona> iterator = llista.iterator();
+        while (iterator.hasNext()) {
+            elem[i] = (Persona)iterator.next();
+            i++;
+        }
+        return elem;
+    }
+
+    //Metode per a obtenir una persona a partir del seu id
+    public Persona buscarPerId(int id) throws ElementNoTrobat {
+        Persona[] persones = elements();
+        for (Persona p : persones){
+            if (p.getId_persona() == id) {
+                return p;
+            }
+        }
+        throw new ElementNoTrobat();
+    }
+
+    //Metode per a obtenir un array amb totes les persones que tenen un pes inferior al valor que es passa per parametre
+    public Persona[] personesPesInferior(int pes) {
+        Persona[] persones = elements();
+        int i = 0;
+        for (Persona p : persones) {
+            if (p.getPes()<pes) {
+                i++;
+            }
+        }
+        System.out.println(persones.length);
+        Persona[] array = new Persona[i];
+        i = 0;
+        for (Persona p : persones){
+                if (p.getPes()<pes) {
+                    array[i] = p;
+                    i++;
+                }
+        }
+        System.out.println(array.length);
+        return array;
+    }
+
+}
